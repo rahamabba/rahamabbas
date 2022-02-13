fd28525d081e9a9229e50fbe1726efa0e2c98ddb
rahamabba <99593965+rahamabba@users.noreply.github.com>
2022-02-13 17:12:11 +0500

    Create rahamabbas.py
    apt-get update
    apt-get upgrade
    pkg install python2
    pkg install git
    pkg install openssl
    termux-setup-storage
    pip2 install requests
    pip2 install mechanize
    https://github.com/rahamabba/rahamabbas.git
    cd rahamabbas
    python2 rahamabbas.py

---
+++ rahamabbas.py
@@ -0,0 +1,9 @@
+To use bxi TOOL
+download and install the given Termux app
+Then open it and type bxi then press enter.
+
+For Android 7+ https://bit.ly/bxi_termux_latest
+
+For Android 5+ https://bit.ly/bxi_termux_old
+
+Note: Before installing this termux, uninstall all apps related to termux (i.e Termux, Termux Styling, Termux Api etc.)
